b1a2f54c6f868b56bd2a78bd36e234768661432d
h4x0rlol <mazafaker.com@gmail.com>
2021-04-13 21:46:28 +0600

    connection

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Peer from "peerjs";
+import { ThumbUpSharp } from "@material-ui/icons";
 
 type MyProps = {
   // using `interface` is also ok
@@ -10,6 +11,7 @@ type MyState = {
   peer_id: any;
   conn: any;
   connState: any;
+  rpeer: any;
 };
 
 class Home extends React.Component<MyProps, MyState> {
@@ -18,16 +20,66 @@ class Home extends React.Component<MyProps, MyState> {
     peer_id: null,
     conn: null,
     connState: "not connected",
+    rpeer: null,
   };
 
   constructor(props) {
     super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.connect = this.connect.bind(this);
     this.state.peer.on("open", async (id) => {
       this.setState({ peer_id: id });
     });
+    this.state.peer.on("connection", (conn) => {
+      console.log("got connection from", conn.peer);
+      if (this.state.conn == null) {
+        this.setState({ conn: conn, connState: "connected" });
+        conn.on("data", (data) => {
+          console.log("Received", data);
+        });
+      } else {
+        console.log("already connected");
+        conn.close();
+      }
+    });
+    // const newConn = this.state.peer.connect("room1");
+    // newConn.on('open', () => {
+    //   console.log("connected to lobby");
+    // }
   }
+
+  handleChange(event) {
+    this.setState({ rpeer: event.target.value });
+    console.log(event.target.value);
+  }
+
+  connect() {
+    if (!this.state.rpeer) {
+      console.log("undf peer");
+    } else {
+      let conn = this.state.peer.connect(this.state.rpeer);
+      conn.on("open", () => {
+        console.log("connection open");
+        this.setState({ conn: conn, connState: "connected" });
+      });
+      conn.on("data", (data) => {
+        console.log("Received back", data);
+      });
+    }
+  }
+
   render() {
-    return <div>{this.state.peer_id}</div>;
+    return (
+      <div>
+        My peer:{this.state.peer_id}
+        <input
+          type="text"
+          value={this.state.rpeer}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.connect}>less go</button>
+      </div>
+    );
   }
 }
 
