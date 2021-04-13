654e83dc9053cf85053e95eaccde7293994108ae
h4x0rlol <mazafaker.com@gmail.com>
2021-04-14 00:45:35 +0600

    useless code deleted

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,28 +1,21 @@
 import React from "react";
 import Peer from "peerjs";
-import { ThumbUpSharp } from "@material-ui/icons";
-// RCE CSS
 import "../assets/styles/main.css";
-// MessageBox component
-import { MessageBox } from "react-chat-elements";
 import { Input } from "react-chat-elements";
 import { Button } from "react-chat-elements";
 import { MessageList } from "react-chat-elements";
 
-type MyProps = {
-  message: string;
-};
+type MyProps = {};
+
 type MyState = {
   peer: Peer;
-  peer_id: any;
-  conn: any;
-  connState: any;
+  peer_id: string;
+  conn: Peer.DataConnection;
   rpeer: any;
   message: string;
   rmessage: string;
-  messages: string[];
-  rmessages: string[];
-  arr1: any;
+  messages: any;
+  onSite: string[];
 };
 
 class Home extends React.Component<MyProps, MyState> {
@@ -30,13 +23,11 @@ class Home extends React.Component<MyProps, MyState> {
     peer: new Peer(),
     peer_id: null,
     conn: null,
-    connState: "not connected",
     rpeer: null,
     message: null,
     rmessage: null,
     messages: [],
-    rmessages: [],
-    arr1: [],
+    onSite: [],
   };
 
   constructor(props) {
@@ -45,30 +36,32 @@ class Home extends React.Component<MyProps, MyState> {
     this.connect = this.connect.bind(this);
     this.handleMessage = this.handleMessage.bind(this);
     this.sendMessage = this.sendMessage.bind(this);
-    this.fullArr = this.fullArr.bind(this);
 
     this.state.peer.on("open", async (id) => {
       this.setState({ peer_id: id });
     });
+
     this.state.peer.on("connection", (conn) => {
       console.log("got connection from", conn.peer);
       if (this.state.conn == null) {
-        this.setState({ conn: conn, connState: "connected" });
+        this.setState({ conn: conn });
         conn.on("data", (data) => {
           this.setState((prevState) => ({
-            rmessages: [...prevState.rmessages, data],
+            messages: [
+              ...prevState.messages,
+              {
+                position: "left",
+                type: "text",
+                text: data,
+              },
+            ],
           }));
-          console.log("Received", this.state.rmessages);
+          console.log("Received", this.state.messages);
         });
       } else {
         console.log("already connected");
-        conn.close();
       }
     });
-    // const newConn = this.state.peer.connect("room1");
-    // newConn.on('open', () => {
-    //   console.log("connected to lobby");
-    // }
   }
 
   handleChange(event) {
@@ -88,45 +81,38 @@ class Home extends React.Component<MyProps, MyState> {
       let conn = this.state.peer.connect(this.state.rpeer);
       conn.on("open", () => {
         console.log("connection open");
-        this.setState({ conn: conn, connState: "connected" });
+        this.setState({ conn: conn });
       });
       conn.on("data", (data) => {
         this.setState((prevState) => ({
-          rmessages: [...prevState.rmessages, data],
+          messages: [
+            ...prevState.messages,
+            {
+              position: "left",
+              type: "text",
+              text: data,
+            },
+          ],
         }));
         console.log("Received back", this.state.messages);
       });
     }
   }
 
-  // Добавлять в массив сообщение сразу при отправке
-  fullArr() {
-    this.state.messages.map((item, i) =>
-      this.setState((prevState) => ({
-        arr1: [
-          ...prevState.arr1,
-          {
-            position: "right",
-            type: "text",
-            text: item,
-          },
-        ],
-      }))
-    );
-    console.log(this.state.arr1);
-    // this.setState({
-    //   messages: [],
-    // });
-  }
-
   sendMessage() {
     if (this.state.conn) {
       this.state.conn.send(this.state.message);
       this.setState((prevState) => ({
-        messages: [...prevState.messages, this.state.message],
+        messages: [
+          ...prevState.messages,
+          {
+            position: "right",
+            type: "text",
+            text: this.state.message,
+          },
+        ],
       }));
-      this.fullArr();
-      console.log("sended", this.state.message);
+      console.log("sended", this.state.messages);
     } else {
       console.log("no con?");
     }
@@ -145,38 +131,13 @@ class Home extends React.Component<MyProps, MyState> {
           <br />
           <button onClick={this.connect}>less go</button>
           <br />
-          {/* <input
-            type="text"
-            value={this.state.message}
-            onChange={this.handleMessage}
-          />
-          <button onClick={this.sendMessage}>less go</button> */}
         </div>
-
         <div className="chatbox">
-          {/* {this.state.messages.map((item, i) => (
-            <MessageBox
-              position={"right"}
-              type={"text"}
-              text={item}
-              // data={this.state.message}
-            />
-          ))}  */}
-
-          {/* {this.state.rmessages.map((item, i) => (
-            <MessageBox
-              position={"left"}
-              type={"text"}
-              text={item}
-              // data={this.state.message}
-            />
-          ))} */}
-
           <MessageList
             className="message-list"
             lockable={true}
             toBottomHeight={"100%"}
-            dataSource={this.state.arr1}
+            dataSource={this.state.messages}
           />
 
           <Input
