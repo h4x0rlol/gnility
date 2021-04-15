04c3d3aae0391a4eceadd47b2ff8c3877a4487eb
h4x0rlol <mazafaker.com@gmail.com>
2021-04-15 18:45:00 +0600

    server update

--- server.ts
+++ server.ts
@@ -1,23 +1,15 @@
 const express = require("express");
 const path = require("path");
-// import Peer from "peerjs";
 
 const LOBBY_NAME = "gnility";
 const app = express();
 
-const frontendServer = app.listen(3000);
+let port = process.env.PORT || 3000;
+
+const frontendServer = app.listen(port);
 
 app.use(express.static(__dirname + "/dist"));
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "index.html"));
 });
-
-// console.log("trying to create lobby");
-// const lobby = new Peer(LOBBY_NAME);
-// lobby.on("open", function (id) {
-//   console.log("Lobby peer ID is: " + id);
-// });
-// lobby.on("connection", function (conn) {
-//   console.log("lobby connection", conn.peer);
-// });
